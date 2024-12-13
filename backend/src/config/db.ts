8dda6af74e047704e9b7618b605b0b5a68c8a1ff
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-12 23:18:57 -0300

    feat: add models configuration and disable logging

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = new Sequelize(process.env.DATABASE_URL, {
+  models: [__dirname + "/../models/**/*"],
+  logging: false, // Elimina el codigo SQL de la consola.
   dialectOptions: {
     ssl: {
       require: false,
