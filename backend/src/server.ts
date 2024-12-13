b3c45279c7c5fe0774be35cc2845207e9eccab90
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-12 23:02:57 -0300

    feat: add database connection with Sequelize

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,19 @@
 import express from "express";
 import colors from "colors";
 import morgan from "morgan";
+import { db } from "./config/db";
+
+async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log(colors.blue.bold("Conexion exitosa a la base de datos"));
+  } catch (error) {
+    console.log(colors.red.bold("Fallo la conexion a la base de datos"));
+  }
+}
+
+connectDB();
 
 const app = express();
 
