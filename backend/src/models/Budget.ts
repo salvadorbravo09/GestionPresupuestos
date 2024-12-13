eba40c48b608ad3e498dee81e3ca256dca833128
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-12 23:21:25 -0300

    feat: add Budget model with name and amount fields

---
+++ backend/src/models/Budget.ts
@@ -0,0 +1,26 @@
+import {
+  Table,
+  Column,
+  DataType,
+  HasMany,
+  BelongsTo,
+  ForeignKey,
+  Model,
+} from "sequelize-typescript";
+
+@Table({
+  tableName: "budgets",
+})
+class Budget extends Model {
+  @Column({
+    type: DataType.STRING(100),
+  })
+  name: string;
+
+  @Column({
+    type: DataType.DECIMAL,
+  })
+  amount: number;
+}
+
+export default Budget;
