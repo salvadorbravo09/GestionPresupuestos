75e654ae7d31999bcc98c9c288d4a982102adfda
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-15 15:18:15 -0300

    feat: add route for confirm-account and add limiter

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { body } from "express-validator";
 import { AuthController } from "../controllers/AuthController";
 import { handleInputErrors } from "../middleware/validation";
+import { limiter } from "../config/limiter";
 
 const router = Router();
 
@@ -14,6 +15,17 @@ router.post(
   body("email").isEmail().withMessage("Email no valido"),
   handleInputErrors,
   AuthController.createAccount
+);
+
+router.post(
+  "/confirm-account",
+  limiter,
+  body("token")
+    .notEmpty()
+    .isLength({ min: 6, max: 6 })
+    .withMessage("Token no valido"),
+  handleInputErrors,
+  AuthController.confirmAccount
 );
 
 export default router;
