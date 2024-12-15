9a977f6dd91e6c10c6423c9d89d877e009b54b0c
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-15 16:15:58 -0300

    feat: add route for login and validations

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -6,6 +6,8 @@ import { limiter } from "../config/limiter";
 
 const router = Router();
 
+router.use(limiter);
+
 router.post(
   "/create-account",
   body("name").notEmpty().withMessage("El nombre no puede ir vacio"),
@@ -19,13 +21,20 @@ router.post(
 
 router.post(
   "/confirm-account",
-  limiter,
   body("token")
     .notEmpty()
     .isLength({ min: 6, max: 6 })
     .withMessage("Token no valido"),
   handleInputErrors,
   AuthController.confirmAccount
+);
+
+router.post(
+  "/login",
+  body("email").isEmail().withMessage("Email no valido"),
+  body("password").notEmpty().withMessage("El password es obligatorio"),
+  handleInputErrors,
+  AuthController.login
 );
 
 export default router;
