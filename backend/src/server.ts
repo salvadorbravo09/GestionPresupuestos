7449b72f4c364a73775ca5272acde05aba77a560
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-14 21:28:08 -0300

    feat: add authentication route to the application

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import colors from "colors";
 import morgan from "morgan";
 import { db } from "./config/db";
 import budgetRouter from "./routes/budgetRouter";
+import authRouter from "./routes/authRouter";
 
 async function connectDB() {
   try {
@@ -23,5 +24,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/api/budgets", budgetRouter);
+app.use("/api/auth", authRouter);
 
 export default app;
