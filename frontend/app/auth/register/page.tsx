2fda0d282722c25d19c6c2998083684dcebc460d
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-23 17:46:33 -0300

    refactor: change design

--- frontend/app/auth/register/page.tsx
+++ frontend/app/auth/register/page.tsx
@@ -8,14 +8,15 @@ export const metadata: Metadata = {
 
 export default function RegisterPage() {
   return (
-    <>
-      <h1 className="font-bold text-5xl text-white mb-4">Regístrate ahora</h1>
-      <p className="text-2xl font-medium text-gray-300">
-        Comienza a gestionar tus{" "}
-        <span className="text-green-400">finanzas</span>
-      </p>
-
-      <RegisterForm />
-    </>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <h1 className="font-bold text-6xl text-white mb-4">Regístrate ahora</h1>
+        <p className="text-lg font-medium text-gray-300">
+          Completa el formulario para crear tu{" "}
+          <span className="text-green-400">cuenta</span>
+        </p>
+        <RegisterForm />
+      </div>
+    </div>
   );
 }
