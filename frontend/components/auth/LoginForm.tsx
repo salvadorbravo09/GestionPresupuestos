778243ad97a75a5555667c30decdf085b116709c
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-23 17:45:19 -0300

    feat: create form for LoginForm

---
+++ frontend/components/auth/LoginForm.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+export default function LoginForm() {
+  return (
+    <>
+      <form className="mt-14 space-y-5" noValidate>
+        <div className="flex flex-col gap-2">
+          <label className="font-bold text-2xl">Email</label>
+
+          <input
+            id="email"
+            type="email"
+            placeholder="Email de Registro"
+            className="w-full border border-gray-300 p-3 rounded-lg"
+            name="email"
+          />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <label className="font-bold text-2xl">Password</label>
+
+          <input
+            type="password"
+            placeholder="Password de Registro"
+            className="w-full border border-gray-300 p-3 rounded-lg"
+            name="password"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Iniciar Sesión"
+          className="bg-green-400 hover:bg-green-500 w-full p-3 rounded-lg text-white font-black  text-xl cursor-pointer"
+        />
+      </form>
+    </>
+  );
+}
