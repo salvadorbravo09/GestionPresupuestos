02b710555f17117a38573f1c83ae747deaba2cc9
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-29 19:43:38 -0300

    feat: create an object to send to the backend

--- frontend/actions/create-account-action.ts
+++ frontend/actions/create-account-action.ts
@@ -1,5 +1,12 @@
 "use server";
 
-export async function register() {
-  console.log("desde server actions");
+export async function register(formData: FormData) {
+  const registerData = {
+    email: formData.get("email"),
+    name: formData.get("name"),
+    password: formData.get("password"),
+    password_confirmation: formData.get("password_confirmation"),
+  };
+
+  console.log(registerData);
 }
